ae28cbfa7793a5a4af498a5d5290100698aa649d
finnformica <finnformica@gmail.com>
2025-09-12 22:58:05 +0100

    seed db with default user account

--- src/lib/db/seed.ts
+++ src/lib/db/seed.ts
@@ -1,7 +1,7 @@
 import { hashPassword } from '@/lib/auth/session';
 import { stripe } from '../payments/stripe';
 import { db } from './config';
-import { teamMembers, teams, users } from './schema';
+import { dimAccount, users } from './schema';
 
 async function createStripeProducts() {
   console.log('Creating Stripe products and prices...');
@@ -44,6 +44,7 @@ async function seed() {
   const password = 'admin123';
   const passwordHash = await hashPassword(password);
 
+  // Create a default user
   const [user] = await db
     .insert(users)
     .values([
@@ -55,20 +56,17 @@ async function seed() {
     ])
     .returning();
 
+    // Create a default account for user
+    await db
+      .insert(dimAccount)
+      .values({
+        userId: user.id,
+        accountName: 'Primary Account',
+        accountType: 'INDIVIDUAL',
+        currency: 'USD',
+      });
+
   console.log('Initial user created.');
-
-  const [team] = await db
-    .insert(teams)
-    .values({
-      name: 'Test Team',
-    })
-    .returning();
-
-  await db.insert(teamMembers).values({
-    teamId: team.id,
-    userId: user.id,
-    role: 'owner',
-  });
 
   // await createStripeProducts();
 }
